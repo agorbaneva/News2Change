dd94b703764f2d76f7726737f5e83aa5dd7d3b9e
Alyssa Gorbaneva <alyssa.gorbaneva@duke.edu>
2022-12-03 22:00:41 -0500

    Update index.js

--- public/index.js
+++ public/index.js
@@ -28,110 +28,102 @@ app.use(express.static('static'));
 // -------------- express 'get' handlers -------------- //
 
 app.get('/', function(req, res){
-
-
 	// render the template
 	res.render('index');
 
 });
 
 app.get('/reps', function(req, res){
+	if('lat' in req.query){
 
+		var lat = Number(req.query.lat);
+		var long = Number(req.query.long);
 
-		if('lat' in req.query){
-
-			var lat = Number(req.query.lat);
-			var long = Number(req.query.long);
-
-			geocoder.reverse(""+lat +"," + long)
-			  .then(response => {
-			     // address = response[0].formatted_address;
-					if(!response || !response.results || !response.results[0]){
-						res.send('<p>Error loading coordinates, most likely outside the US</p>');
-					}
-					var actualAddress = response.results[0].formatted_address;
-					const search = ' ';
-				 	const replaceWith = '%20';
-					var address = actualAddress.split(search).join(replaceWith);
-					console.log(address)
-
-				
-			    var url = "www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyCrL1K45L0blXvtPod35xY1Bo0-qBJAOOc&address=" + address
-			    console.log(url)
-
-			    const options = {
-				  hostname: 'www.googleapis.com',
-				  port: 443,
-				  path: '/civicinfo/v2/representatives?key=AIzaSyCrL1K45L0blXvtPod35xY1Bo0-qBJAOOc&address=' + address,
-				  method: 'GET',
-				  headers: {
-				    'Accept': 'plain/html',
-				    'Accept-Encoding': '*',
-				  }
+		geocoder.reverse(""+lat +"," + long)
+		  .then(response => {
+			 // address = response[0].formatted_address;
+				if(!response || !response.results || !response.results[0]){
+					res.send('<p>Error loading coordinates, most likely outside the US</p>');
 				}
+				var actualAddress = response.results[0].formatted_address;
+				const search = ' ';
+				const replaceWith = '%20';
+				var address = actualAddress.split(search).join(replaceWith);
+				console.log(address)
 
 
-			    		https.get(options, function(response) {
+			var url = "www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyCrL1K45L0blXvtPod35xY1Bo0-qBJAOOc&address=" + address
+			console.log(url)
+
+			const options = {
+			  hostname: 'www.googleapis.com',
+			  port: 443,
+			  path: '/civicinfo/v2/representatives?key=AIzaSyCrL1K45L0blXvtPod35xY1Bo0-qBJAOOc&address=' + address,
+			  method: 'GET',
+			  headers: {
+				'Accept': 'plain/html',
+				'Accept-Encoding': '*',
+			  }
+			}
 
 
-							var rawData = '';
+					https.get(options, function(response) {
 
-							response.on('data', function(chunk) {
-						    	rawData += chunk;
 
-						    });
+						var rawData = '';
 
-							response.on('end', function() {
+						response.on('data', function(chunk) {
+							rawData += chunk;
 
-							  obj = JSON.parse(rawData);
-							  var senator1 = obj.officials[2]
-							  var senator2 = obj.officials[3]
-							  var local_rep = obj.officials[4]
-							  var governor = obj.officials[5]
-
-								const download = (url, path, callback) => {
-								  request.head(url, (err, res, body) => {
-								    request(url)
-								      .pipe(fs.createWriteStream(path))
-								      .on('close', callback)
-								  })
-								}
-
-								const reps = [senator1, senator2, local_rep, governor]
-								const titles = ["Senator 1", "Senator 2", "House of Representatives", "Governor"]
-								const paths = ['./static/img/senator1.png', './static/img/senator2.png', './static/img/local_rep.png', './static/img/governor.png']
-
-								out = {address: actualAddress, representatives:[]}
-
-								reps.forEach(function (rep, i) {
-									var item = {name:rep.name, 
-															photoUrl:rep.photoUrl ? rep.photoUrl : "https://upload.wikimedia.org/wikipedia/commons/4/4b/Seal_of_the_United_States_Congress.svg", 
-															party:rep.party, 
-															site:rep.urls[0],
-															email: rep.emails ? rep.emails[0] : null,
-															position:titles[i]}
-									out.representatives.push(item)
-									// out.push({
-									// 	""+reps[i]: rep.name 
-									// 	optionValue: status.options[i].value
-									// });
-						
-								});
-								res.render("reps", {object: out})
 						});
 
-					}).on('error', function(e){
-						console.error(e);
+						response.on('end', function() {
 
-					}).end();
+						  obj = JSON.parse(rawData);
+						  var senator1 = obj.officials[2]
+						  var senator2 = obj.officials[3]
+						  var local_rep = obj.officials[4]
+						  var governor = obj.officials[5]
 
-			  })
-			  .catch(err => {
-			    console.error(err);
-			  }
-			);
+							const download = (url, path, callback) => {
+							  request.head(url, (err, res, body) => {
+								request(url)
+								  .pipe(fs.createWriteStream(path))
+								  .on('close', callback)
+							  })
+							}
 
-		}
+							const reps = [senator1, senator2, local_rep, governor]
+							const titles = ["Senator 1", "Senator 2", "House of Representatives", "Governor"]
+							const paths = ['./static/img/senator1.png', './static/img/senator2.png', './static/img/local_rep.png', './static/img/governor.png']
+
+							out = {address: actualAddress, representatives:[]}
+
+							reps.forEach(function (rep, i) {
+								var item = {name:rep.name, 
+												photoUrl:rep.photoUrl ? rep.photoUrl : "https://upload.wikimedia.org/wikipedia/commons/4/4b/Seal_of_the_United_States_Congress.svg", 
+												party:rep.party, 
+												site:rep.urls[0],
+												email: rep.emails ? rep.emails[0] : null,
+												position:titles[i]}
+								out.representatives.push(item)
+
+							});
+							res.render("reps", {object: out})
+					});
+
+				}).on('error', function(e){
+					console.error(e);
+
+				}).end();
+
+		  })
+		  .catch(err => {
+			console.error(err);
+		  }
+		);
+
+	}
 });
  
 
